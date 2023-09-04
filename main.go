dc64f2c900e36640b21eea67b6a25f954bca8615
WatchJani <jankokondic84@gmail.com>
2023-09-04 20:32:50 +0200

    top

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+type Flag struct {
+	key         string
+	defaultKey  string
+	description string
+}
+
+type Command struct {
+	keyWord string
+	flags   []Flag
+}
+
+func FlagCommand(command string) *Command {
+	return &Command{
+		keyWord: command,
+	}
+}
+
+func (c *Command) AddFlag(flags ...Flag) {
+	c.flags = append(c.flags, flags...)
+}
+
+func (c Command) Parse() {
+
+}
+
+func main() {
+	add := FlagCommand("add")
+
+	add.Parse()
+}
