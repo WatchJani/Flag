2a305a819503767fdc0fa7aeb270118e7b041336
WatchJani <jankokondic84@gmail.com>
2023-09-08 20:22:38 +0200

    finish

--- main.go
+++ main.go
@@ -4,114 +4,21 @@ import (
 	"fmt"
 	"os"
 	"reflect"
+	"strings"
 )
 
-// type Flag struct {
-// 	key          string
-// 	defaultValue string
-// 	description  string
-// }
-
-// func NewFlag(key, defaultValue, description string) *Flag {
-// 	return &Flag{
-// 		key:          key,
-// 		defaultValue: defaultValue,
-// 		description:  description,
-// 	}
-// }
-
-// type Command struct {
-// 	command map[string]func(...Flag)
-// 	flags   map[string][]Flag
-// }
-
-// func New() *Command {
-// 	return &Command{
-// 		command: make(map[string]func(...Flag)),
-// 		flags:   make(map[string][]Flag),
-// 	}
-// }
-
-// func (c *Command) Add(keyWord string, fn func(...Flag), flags ...Flag) {
-// 	c.command[keyWord] = fn
-// 	c.flags[keyWord] = append(c.flags[keyWord], flags...)
-// }
-
-// func (c Command) Parse() {
-// 	args := os.Args[1:]
-// 	if len(args) < 1 {
-// 		return
-// 	}
-
-// 	c.command[args[0]](c.flags[args[0]]...)
-// }
-
-// func main() {
-// 	command := New()
-
-// 	p := NewFlag("p", "", "description test")
-
-// 	command.Add("read", Read, *p)
-// 	command.Add("write", Write)
-
-// 	command.Parse()
-// }
-
-// func Read(flag ...Flag) {
-// 	fmt.Println("Read", " ", flag)
-// }
-
-// func Write(flag ...Flag) {
-// 	fmt.Println("Write")
-// }
-
-//====================================================================================
-// func Read(a int, b string, c float64) {
-// 	// Vaša funkcija Read može koristiti a, b i c kao zasebne argumente
-// 	fmt.Printf("a: %d, b: %s, c: %f\n", a, b, c)
-// }
-
-// func CallWithSliceData(fn interface{}, args []interface{}) {
-// 	// Provjerite da li funkcija ima ispravan broj parametara
-// 	funcType := reflect.TypeOf(fn)
-// 	if funcType.Kind() != reflect.Func || funcType.NumIn() != len(args) {
-// 		fmt.Println("Pogrešan broj argumenata.")
-// 		return
-// 	}
-
-// 	// Pripremite argumente za poziv funkcije
-// 	callArgs := make([]reflect.Value, len(args))
-// 	for i, arg := range args {
-// 		callArgs[i] = reflect.ValueOf(arg)
-// 	}
-
-// 	// Pozovite funkciju s argumentima
-// 	reflect.ValueOf(fn).Call(callArgs)
-// }
-
-// func main() {
-// 	data := []interface{}{42, "Hello", 3.14}
-
-// 	// Automatizirani poziv funkcije Read s elementima iz slice-a
-// 	CallWithSliceData(Read, data)
-// }
-//=======================================================================================
-
 func CallWithSliceData(fn interface{}, args []interface{}) {
-	// Provjerite da li funkcija ima ispravan broj parametara
 	funcType := reflect.TypeOf(fn)
 	if funcType.Kind() != reflect.Func || funcType.NumIn() != len(args) {
-		fmt.Println("Pogrešan broj argumenata.")
+		fmt.Println("The wrong arguments.")
 		return
 	}
 
-	// Pripremite argumente za poziv funkcije
 	callArgs := make([]reflect.Value, len(args))
 	for i, arg := range args {
 		callArgs[i] = reflect.ValueOf(arg)
 	}
 
-	// Pozovite funkciju s argumentima
 	reflect.ValueOf(fn).Call(callArgs)
 }
 
@@ -142,6 +49,8 @@ func New() *Command {
 }
 
 func (c *Command) Add(keyWord string, fn interface{}, flags ...Flag) {
+	keyWord = strings.ToLower(keyWord)
+
 	c.command[keyWord] = fn
 	c.flags[keyWord] = append(c.flags[keyWord], flags...)
 }
@@ -159,11 +68,9 @@ func (c Command) Parse() {
 	}
 
 	CallWithSliceData(c.command[args[0]], flags)
-	// c.command[args[0]](c.flags[args[0]]...)
 }
 
 func main() {
-
 	command := New()
 
 	a := NewFlag("a", 0, "my Super int")
@@ -171,30 +78,19 @@ func main() {
 	c := NewFlag("c", 3.14, "my Super bool")
 
 	command.Add("read", Read, a, b, c)
-	// command.Add("write", AnotherFunction)
+
+	x := NewFlag("x", "mojX", "How is work:D")
+	y := NewFlag("y", true, "just true")
+
+	command.Add("anotherFunction", AnotherFunction, x, y)
 
 	command.Parse()
-
-	// data1 := []interface{}{42, "Hello", 3.14}
-	// data2 := []interface{}{"World", true}
-
-	// fmt.Println(data1)
-
-	// functionMap := make(map[string]interface{})
-	// functionMap["Read"] = Read
-	// functionMap["AnotherFunction"] = AnotherFunction
-
-	// // Automatizirani poziv funkcija iz mape s elementima iz slice-a
-	// CallWithSliceData(functionMap["Read"], data1)
-	// CallWithSliceData(functionMap["AnotherFunction"], data2)
 }
 
 func Read(a int, b string, c float64) {
-	// Vaša funkcija Read može koristiti a, b i c kao zasebne argumente
 	fmt.Printf("a: %d, b: %s, c: %f\n", a, b, c)
 }
 
 func AnotherFunction(x string, y bool) {
-	// Funkcija s drugim argumentima
 	fmt.Printf("x: %s, y: %v\n", x, y)
 }
